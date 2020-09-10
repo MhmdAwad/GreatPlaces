2148954eee0925e8782bfa3f16fd7857e9f56136
MhmdAwad <mhmdawaddd@gmail.com>
2020-09-10 19:40:50 +0200

    split addPlaceScreen to another widget to save image path.

--- lib/Screens/AddNewPlaceScreen.dart
+++ lib/Screens/AddNewPlaceScreen.dart
@@ -2,9 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path/path.dart' as path;
-import 'package:path_provider/path_provider.dart' as pathProv;
+import 'package:great_places/Widgets/ImageSelector.dart';
 
 class AddPlacesScreen extends StatefulWidget {
   static const ROUTE_NAME = "AddPlacesScreen";
@@ -14,18 +12,17 @@ class AddPlacesScreen extends StatefulWidget {
 }
 
 class _AddPlacesScreenState extends State<AddPlacesScreen> {
-  File _imageFile;
+  File _pickedImage;
+  TextEditingController _titleController;
 
-  void _takePic() async {
-    final imageFile =
-        await ImagePicker().getImage(source: ImageSource.camera, maxWidth: 600);
-    setState(() {
-      _imageFile = File(imageFile.path);
-    });
-    final appDir = await pathProv.getApplicationDocumentsDirectory();
-    final fileName = path.basename(_imageFile.path);
-    print("${appDir.path} === $fileName");
-    final savedImage = await _imageFile.copy("${appDir.path}/$fileName");
+  void _saveImage(File selectedImage) {
+    _pickedImage = selectedImage;
+  }
+  void _savePlace(){
+    if(_titleController.text.isEmpty || _pickedImage == null)
+      return;
+
+
   }
 
   @override
@@ -44,46 +41,12 @@ class _AddPlacesScreenState extends State<AddPlacesScreen> {
                     children: [
                       TextFormField(
                         decoration: InputDecoration(labelText: "Title"),
+                        controller: _titleController,
                       ),
                       SizedBox(
                         height: 20,
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            alignment: Alignment.center,
-                            width: 100,
-                            height: 100,
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 1, color: Colors.grey),
-                            ),
-                            child: _imageFile == null
-                                ? Text(
-                                    "No Image!",
-                                    textAlign: TextAlign.center,
-                                  )
-                                : Image.file(
-                                    _imageFile,
-                                    fit: BoxFit.cover,
-                                    width: double.infinity,
-                                  ),
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: FlatButton.icon(
-                              icon: Icon(
-                                Icons.camera,
-                              ),
-                              label: Text("Select Image"),
-                              textColor: Theme.of(context).primaryColor,
-                              onPressed: _takePic,
-                            ),
-                          )
-                        ],
-                      ),
+                      ImageSelector(_saveImage)
                     ],
                   )),
               RaisedButton(
---
+++ lib/Widgets/ImageSelector.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as pathProv;
+
+class ImageSelector extends StatefulWidget {
+  final Function saveImageFun;
+  ImageSelector(this.saveImageFun);
+
+  @override
+  _ImageSelectorState createState() => _ImageSelectorState();
+}
+
+class _ImageSelectorState extends State<ImageSelector> {
+  File _imageFile;
+
+  void _takePic() async {
+    final imageFile =
+    await ImagePicker().getImage(source: ImageSource.camera, maxWidth: 600);
+    setState(() {
+      _imageFile = File(imageFile.path);
+    });
+    final appDir = await pathProv.getApplicationDocumentsDirectory();
+    final fileName = path.basename(_imageFile.path);
+    print("${appDir.path} === $fileName");
+    final savedImage = await _imageFile.copy("${appDir.path}/$fileName");
+    widget.saveImageFun(savedImage);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Container(
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _imageFile == null
+              ? Text(
+            "No Image!",
+            textAlign: TextAlign.center,
+          )
+              : Image.file(
+            _imageFile,
+            fit: BoxFit.cover,
+            width: double.infinity,
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            icon: Icon(
+              Icons.camera,
+            ),
+            label: Text("Select Image"),
+            textColor: Theme
+                .of(context)
+                .primaryColor,
+            onPressed: _takePic,
+          ),
+        )
+      ],
+    );
+  }
+}
